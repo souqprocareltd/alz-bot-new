15d32144164c30ff51168dde3d4ad1bd9fe1b04e
souqprocareltd <141635285+souqprocareltd@users.noreply.github.com>
2023-11-14 13:44:23 +0200

    Update linke-hader.php

--- fi-t/linke-hader.php
+++ fi-t/linke-hader.php
@@ -1,7 +1,7 @@
 <link href="https://alz-bot.com/" rel="canonical">
      <link href="https://alz-bot.com/" rel="home">
      <link rel="stylesheet" href="them-viz/css/bootstrap-grid.css" type="text/css" media="all">
-     <link rel="stylesheet" href="them-viz/css/style.css" type="text/css" media="all">
+     <link rel="stylesheet" href="them-viz/css/styles.css" type="text/css" media="all">
      <link rel="stylesheet" href="lang/ar/css/alz-bot.css" type="text/css" media="all">
      <link rel="stylesheet" href="them-viz/css/glide.css">
      <link rel="stylesheet" href="them-viz/css-viz/style-pricing.css">  
